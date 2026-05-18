ee74c54223bec8432c48a64b17f11040863aa056
Marcelo Primi <marceloprimi@gmail.com>
2026-05-18 18:02:04 -0300

    chore: rodar migrations automaticamente no startup

--- startup.py
+++ startup.py
@@ -1,6 +1,11 @@
 import os, json
+from alembic.config import Config
+from alembic import command
 
 sa_json = os.getenv("GOOGLE_SERVICE_ACCOUNT_JSON")
 if sa_json:
     with open("service_account.json", "w") as f:
         json.dump(json.loads(sa_json), f)
+
+alembic_cfg = Config("alembic.ini")
+command.upgrade(alembic_cfg, "head")
