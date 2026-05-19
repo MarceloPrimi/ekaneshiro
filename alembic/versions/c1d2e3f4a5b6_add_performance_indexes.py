eafd28f7fecf8c2416fc837c0e112fbfb34f1ded
Marcelo Primi <marceloprimi@gmail.com>
2026-05-19 10:14:22 -0300

    fix: corrige down_revision da migration de indices

--- alembic/versions/c1d2e3f4a5b6_add_performance_indexes.py
+++ alembic/versions/c1d2e3f4a5b6_add_performance_indexes.py
@@ -11,7 +11,7 @@ from alembic import op
 
 
 revision: str = 'c1d2e3f4a5b6'
-down_revision: Union[str, None] = 'f1e2d3c4b5a6'
+down_revision: Union[str, None] = 'b2c3d4e5f6a7'
 branch_labels: Union[str, Sequence[str], None] = None
 depends_on: Union[str, Sequence[str], None] = None
 
